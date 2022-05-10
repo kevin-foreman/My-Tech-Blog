07702b48a3fe228678b69dbf432bbed7d91528da
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-05-10 14:20:24 -0500

    User model completed with table definitions

--- models/User.js
+++ models/User.js
@@ -8,6 +8,32 @@ User.init(
     {
 
         // TABLE column definitions
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
+        }
 
     },
     {
