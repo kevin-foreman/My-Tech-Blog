9c49e9f3ea252890be8a902d23b312f9b9f5aa85
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-05-10 12:04:38 -0500

    Begin server.js to define all dependencies

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+const helpers = require('./utils/helpers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'IhaveAsecret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
