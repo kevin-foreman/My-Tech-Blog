4617b3a2acea2a5e3c63a1f4216014ebb64d52ab
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-05-11 15:53:30 -0500

    branch structure recreated, now finding a bug in handlebars stopping the page from rendering

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,15 @@
 // Import the sequelize constructor
-
 const Sequelize = require('sequelize');
+
 require('dotenv').config();
 
-// Create connection to the database from schema file
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-});
+// create connection to our db
+const sequelize = process.env.JAWSDB_URL
+    ? new Sequelize(process.env.JAWSDB_URL)
+    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
 
 module.exports = sequelize;
