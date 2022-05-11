689bba8aabd38245ed5a94c4302f78b24ab2a869
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-05-11 12:05:09 -0500

    Changed structure of attributes to include Post Content

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -6,7 +6,7 @@ router.get('/', (req, res) => {
     console.log('=======/\========/\=======');
     Post.findAll({
         // Query configuration
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: ['id', 'post_content', 'title', 'created_at'],
         order: [['created_at', 'DESC']],
 
         include: [
@@ -30,7 +30,7 @@ router.get('/:id', (req, res) => {
         where: {
             id: req.params.id
         },
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: ['id', 'post_content', 'title', 'created_at'],
         include: [
             {
                 model: User,
@@ -53,10 +53,10 @@ router.get('/:id', (req, res) => {
 
 // Route to post to the database
 router.post('/', (req, res) => {
-    // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
+    // expects {title: 'Taskmaster goes public!', post_content: 'https://taskmaster.com/press', user_id: 1}
     Post.create({
         title: req.body.title,
-        post_url: req.body.post_url,
+        post_content: req.body.post_content,
         user_id: req.body.user_id
     })
         .then(dbPostData => res.json(dbPostData))
--- models/index.js
+++ models/index.js
@@ -12,5 +12,4 @@ Post.belongsTo(User, {
 });
 
 
-
 module.exports = { User, Post };
