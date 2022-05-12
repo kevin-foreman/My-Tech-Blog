e461c4441b3928de17b0e93b77688d46c615ff44
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-05-12 16:26:56 -0500

    Added clickable Home button and Dashboard buttons

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -34,6 +34,41 @@ router.get('/', (req, res) => {
         });
 });
 
+// return a single post
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'post_content',
+            'title',
+            'created_at'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+            // serialize the data
+            const post = dbPostData.get({ plain: true });
+            // pass data to template
+            res.render('single-post', { post });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
 // Redirect to the homepage if a user is logged in (session detected)
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
